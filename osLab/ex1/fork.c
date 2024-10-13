da801c62da1155ea0ae4f7ad701f77058e6c3843
Nikolaos Papakonstantopoulos <nikolaospapa3@gmail.com>
2024-10-13 10:02:25 +0300

    Create fork.c

---
+++ osLab/ex1/fork.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+
+	pid_t pid = getpid();
+	pid_t desired_pid = 32767;
+		
+	char pathname[]="./riddle";
+        char *argv[]={pathname, NULL};
+        char *envp[]={NULL};
+
+	if(pid == desired_pid){
+		execve(pathname, argv, envp);
+	}
+
+	while(pid != desired_pid){
+        	
+		if((pid=fork())<0) {
+                	perror("fork");
+                	exit(1);
+        	}
+
+		if(pid==0){ // the child
+			if(getpid() == desired_pid)
+				execve(pathname, argv, envp);
+			else
+				exit(0);
+		}
+	}
+
+return 0;
+
+}
