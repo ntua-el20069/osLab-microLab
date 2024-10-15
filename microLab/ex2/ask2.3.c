657f3370f032c6ce92a4eef968d102d6b7f4f9e8
Nikolaos Papakonstantopoulos <nikolaospapa3@gmail.com>
2024-10-16 00:08:18 +0300

    Create ask2.3.c

---
+++ microLab/ex2/ask2.3.c
@@ -0,0 +1,42 @@
+#define F_CPU 16000000UL
+#include<avr/io.h>
+#include<util/delay.h>
+#include<avr/interrupt.h>
+
+int active = 0; // is active = true that mean PB0 is on for 5 seconds.
+// only when light PB0 is already on, should we light up all lights for 0.5sec
+int t = 10; // counter for timer (10 x 500ms = 5 seconds)
+
+
+ISR (INT1_vect) {
+    sei(); // enable interrupts again
+    if (active == 1) {
+        t = 10; // renew time...
+        PORTB = 0xFF;
+        _delay_ms(500); // all leds should light up for 500ms
+    }
+    PORTB = 0x01 ; // only PB0 should be on...
+    active = 1;
+    while(t > 0) {
+     _delay_ms(500); // delay 500ms
+     t -= 1;
+    }
+    active = 0;
+    t = 10;
+    PORTB = 0x00; //turn of the lights
+   }
+
+
+int main(int argc, char** argv) {
+    EICRA = (1 << ISC10) | (1 << ISC11);
+    EIMSK = (1 << INT1);
+    sei();
+    
+    DDRB = 0xFF; // set PORTB as output
+    
+    while (1) {
+    ;
+    }
+     
+    return (0);
+}
