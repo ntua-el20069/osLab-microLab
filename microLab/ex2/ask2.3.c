83110f20666b1ffecc44b9a628b60efad0e4490f
FilotheiLinardatou <120746033+FilotheiLinardatou@users.noreply.github.com>
2024-10-18 14:26:18 +0300

    Update ask2.3.c
    updated code

--- microLab/ex2/ask2.3.c
+++ microLab/ex2/ask2.3.c
@@ -6,16 +6,24 @@
 int active = 0; // is active = true that mean PB0 is on for 5 seconds.
 // only when light PB0 is already on, should we light up all lights for 0.5sec
 int t = 10; // counter for timer (10 x 500ms = 5 seconds)
+int flag=0;
 
 
 ISR (INT1_vect) {
     sei(); // enable interrupts again
-    if (active == 1) {
+     if(active==1 && flag==1){ _delay_ms(500); return;}// all leds should light up for 500ms}
+    if (active == 1) { //anameno to 1 LED
         t = 10; // renew time...
+        
         PORTB = 0xFF;
-        _delay_ms(500); // all leds should light up for 500ms
+        flag=1;
+        _delay_ms(500);
+        flag=0;
+       
     }
+   
     PORTB = 0x01 ; // only PB0 should be on...
+   
     active = 1;
     while(t > 0) {
      _delay_ms(500); // delay 500ms
