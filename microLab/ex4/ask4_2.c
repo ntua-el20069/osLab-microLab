af04095b423169702b277e6d5e28fa98daaf2c6c
FilotheiLinardatou <120746033+FilotheiLinardatou@users.noreply.github.com>
2024-10-29 12:39:51 +0200

    Add files via upload

---
+++ microLab/ex4/ask4_2.c
@@ -0,0 +1,168 @@
+#define F_CPU 16000000UL
+#include "avr/io.h"
+#include <util/delay.h>
+
+void lcd_data(int data){
+    PORTD|=(1<<PD2);
+   
+    int status=write_2_nibbles(data);
+      
+      _delay_ms(0.25);  //1usec=0.001msec
+}
+void lcd_init(){
+
+     _delay_ms(200);
+     
+      PORTD=0x30;       //command to switch to 8 bit mode
+      PORTD|=(1<<PD3);  //Enable pulse
+       
+      //_delay_ms(0.000125); 2 nop -> 0.125 usec
+      
+      __asm__ __volatile__ ("nop");
+      __asm__ __volatile__ ("nop");
+  //__asm__ tells the compiler to insert an assembly instruction.
+//_volatile__ prevents the compiler from optimizing away this statement, ensuring that the NOP operation is indeed executed
+
+      PORTD &=0b11110111;  // Clear PD3
+      
+      _delay_ms(0.25);
+      
+       PORTD=0x30;       //command to switch to 8 bit mode
+      PORTD|=(1<<PD3);  //Enable pulse
+          __asm__ __volatile__ ("nop");
+      __asm__ __volatile__ ("nop");
+     
+      PORTD &=0b11110111;  // Clear PD3
+      
+      _delay_ms(0.25);
+      
+         PORTD=0x30;       //command to switch to 8 bit mode
+      PORTD|=(1<<PD3);  //Enable pulse
+          __asm__ __volatile__ ("nop");
+      __asm__ __volatile__ ("nop");
+     
+      PORTD &=0b11110111;  // Clear PD3
+      
+      _delay_ms(0.25);
+      
+      
+       PORTD=0x20;       //command to switch to 4 bit mode
+      PORTD|=(1<<PD3);  //Enable pulse
+            __asm__ __volatile__ ("nop");
+      __asm__ __volatile__ ("nop");
+      
+      PORTD &=0b11110111;  // Clear PD3
+      
+      _delay_ms(0.25);
+      
+      int status;
+      
+      status=lcd_command(0x28); //5x8 dots ,2 lines
+      
+
+       status=lcd_command(0x0c); //display on ,cursor off
+       
+       return ;
+}
+
+int lcd_command(int a){
+    PORTD &=0b11111011 ;  //PD2 0 ->instruction
+     int status;
+     
+     status=write_2_nibbles(a);
+    
+     _delay_ms(0.25);
+    return 1;
+}
+
+int write_2_nibbles(int a){
+
+    int input=PIND ;  
+    int first_nibble=a;
+    int second_nibble;
+    
+    input&=0x0f;   //we have to keep PD[3:0] because PD2->RS and PD3->Enable
+   
+    first_nibble&=0xf0;
+    
+    first_nibble=input + first_nibble;
+    
+    PORTD=first_nibble;
+    
+    PORTD|=(1<<PD3); //enable pulse
+    
+        __asm__ __volatile__ ("nop");
+      __asm__ __volatile__ ("nop");
+      
+    PORTD &=0b11110111;  // Clear PD3
+    
+    second_nibble=a;
+    second_nibble=(second_nibble << 4) | (second_nibble >> 4); //swaps the nibbles
+    second_nibble&=0xf0;
+    second_nibble=input +second_nibble;
+    
+    PORTD=second_nibble;
+    
+    PORTD|=(1<<PD3); //enable pulse
+    __asm__ __volatile__ ("nop");
+      __asm__ __volatile__ ("nop");
+      
+    PORTD &=0b11110111;  // Clear PD3
+
+    return 1;
+}
+
+int lcd_clear_display(){
+
+    int status=lcd_command(0x01);
+    _delay_ms(5);    
+    return 1;
+}
+void main(void) {
+    int adc,div_res,mod_res,first_decimal,second_decimal;
+       ADMUX = 0b01000001;     // REFSn[7:6] = 01 -> Vref => AVcc with external capacitor at AREF pin 
+                            // ADLAR[5] = 0 -> right adjusted output
+                            //  MUX[3:0] = 0001 for channel ADC1
+    
+      ADCSRA = 0b10000111;    // ADEN[7] = 1 -> enable ADC
+                            // ADSC[6] = 0 -> no conversion
+                            // ADIE[3] = 0 -> disable ADC interrupt
+                            // ADPS[2:0] = 111 -> Division factor 128 so fADC = 16MHz/128 = 125kHz
+    
+      DDRD=0xFF;         //PORTD output
+      lcd_init();
+      
+      while(1){
+      
+    ADCSRA|=(1<<ADSC);  //set ADSC flag to start
+    
+    int flag =ADCSRA & (1<<ADSC);
+    
+    while(flag!=0){
+    
+        flag=ADCSRA & (1<<ADSC);
+    }  
+    
+      adc =  ADCL + ADCH*256  ;   // read from adc (right adjusted)
+                                    // we should firstly read Low and then High
+                                    // else it does not give expected output
+    
+      adc=adc*5;
+      
+      div_res=adc/1024;            // integer part of Vin
+      mod_res=adc%1024;           
+      
+      mod_res=mod_res*100;       //multiply x 100 to find the 2 decimal digits
+      mod_res=mod_res/1024;
+      
+      first_decimal=mod_res/10;
+      second_decimal=mod_res%10;
+     
+      lcd_clear_display();
+      lcd_data(div_res);
+      lcd_data('.');           // i am not sure if it's ok
+      lcd_data(first_decimal);
+      lcd_data(second_decimal);
+      
+}
+}
