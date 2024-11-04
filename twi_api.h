7174aae1db1a8039ad5f934b03e0acf619ba04ec
Nikolaos Papakonstantopoulos <nikolaospapa3@gmail.com>
2024-11-04 19:18:57 +0200

    Create twi_api.h header file

---
+++ twi_api.h
@@ -0,0 +1,114 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+//#include <xc.h> // include processor files - each processor file is guarded.  
+
+// TODO Insert appropriate #include <>
+
+
+// TODO Insert C++ class definitions if appropriate
+
+
+
+// TODO Insert declarations
+// PCA9555 REGISTERS
+typedef enum {
+ REG_INPUT_0 = 0,
+ REG_INPUT_1 = 1,
+ REG_OUTPUT_0 = 2,
+ REG_OUTPUT_1 = 3,
+ REG_POLARITY_INV_0 = 4,
+ REG_POLARITY_INV_1 = 5,
+ REG_CONFIGURATION_0 = 6,
+ REG_CONFIGURATION_1 = 7
+} PCA9555_REGISTERS;
+
+// Comment a function and leverage automatic documentation with slash star star
+/**
+    <p><b>Function prototype:</b></p>
+  
+    <p><b>Summary:</b></p>
+
+    <p><b>Description:</b></p>
+
+    <p><b>Precondition:</b></p>
+
+    <p><b>Parameters:</b></p>
+
+    <p><b>Returns:</b></p>
+
+    <p><b>Example:</b></p>
+    <code>
+ 
+    </code>
+
+    <p><b>Remarks:</b></p>
+ */
+// TODO Insert declarations or function prototypes (right here) to leverage 
+// live documentation
+
+void twi_init(void);
+
+unsigned char twi_readAck(void);
+
+unsigned char twi_readNak(void);
+
+unsigned char twi_start(unsigned char address);
+
+void twi_start_wait(unsigned char address);
+
+unsigned char twi_write( unsigned char data );
+
+unsigned char twi_rep_start(unsigned char address);
+
+void twi_stop(void);
+
+void PCA9555_0_write(PCA9555_REGISTERS reg, uint8_t value);
+
+uint8_t PCA9555_0_read(PCA9555_REGISTERS reg);
+
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
