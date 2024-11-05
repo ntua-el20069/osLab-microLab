ee1aafabb0b14595c750ae7c3f7cb0ddf7f87a7f
Nikolaos Papakonstantopoulos <nikolaospapa3@gmail.com>
2024-11-05 16:47:14 +0200

    fixed F_CPU and IO1_0 (now working)
    fixed F_CPU before include of <util/delay.h>
    and write to IO1_0

--- microLab/ex5/ex_5_2.c
+++ microLab/ex5/ex_5_2.c
@@ -1,8 +1,6 @@
-// main code not tested 
-
+#define F_CPU 16000000UL
 #include<util/delay.h>
 #include "twi_api.h"
-#define F_CPU 16000000UL
 
 #define LED_PD4_ON 0x01       // connected to IO0_0
 #define LED_PD5_ON 0x02       // connected to IO0_1
@@ -19,36 +17,37 @@ int main(void) {
  twi_init();
  PCA9555_0_write(REG_CONFIGURATION_0, 0x00); //Set EXT_PORT0 as output (I need IO0_0 -> IO0_3 as output ports) 
  PCA9555_0_write(REG_CONFIGURATION_1, 0xF0); // IO1_4 -> IO1_7 as input pins, IO1_0 as output port
+ 
+ PCA9555_0_write(REG_OUTPUT_1, 0x00); // logic 0 written to IO1_0 in order to compare with keyboard inputs
+ 
  uint8_t keyboard_input;
  
  while(1)
  {
 
- // maybe a small delay here or not ? 
- // _delay_ms(50);
- keyboard_input = PCA9555_0_read(REG_OUTPUT_1);
+ keyboard_input = PCA9555_0_read(REG_INPUT_1);
  
  // if "*" seen, then LED PD4 on
- if((keyboard_input & STAR_MASK) == STAR_MASK)      // I supposed that pressed means 1 (I do not know exactly)
+ if((keyboard_input & STAR_MASK) == 0x00)      // pressed means 0 (connected to IO1_0)
     PCA9555_0_write(REG_OUTPUT_0, LED_PD4_ON);
 
  
  // if "0" seen, then LED PD5 on
- else if((keyboard_input & ZERO_MASK) == ZERO_MASK)
+ else if((keyboard_input & ZERO_MASK) == 0x00)
     PCA9555_0_write(REG_OUTPUT_0, LED_PD5_ON);
 
  
  // if "#" seen, then LED PD6 on
- else if((keyboard_input & HASHTAG_MASK) == HASHTAG_MASK)
+ else if((keyboard_input & HASHTAG_MASK) == 0x00)
     PCA9555_0_write(REG_OUTPUT_0, LED_PD6_ON);
 
  
  // if "D" seen, then LED PD7 on
- else if((keyboard_input & D_MASK) == D_MASK)
+ else if((keyboard_input & D_MASK) == 0x00)
     PCA9555_0_write(REG_OUTPUT_0, LED_PD7_ON);
 
  // else keep all leds off
  else PCA9555_0_write(REG_OUTPUT_0, 0x00);
- 
+
  }
 }
