4f4f79fbd775852b77813d418cb6d848e027673f
Nikolaos Papakonstantopoulos <nikolaospapa3@gmail.com>
2024-11-19 15:02:37 +0200

    Update DS1820_api.c

--- microLab/ex7/DS1820_api.c
+++ microLab/ex7/DS1820_api.c
@@ -1,17 +1,18 @@
+#include<stdint.h>
+#define F_CPU 16000000UL
 #include<util/delay.h>
 #include "avr/io.h"
 #include "DS1820_api.h"
 
-
 uint8_t one_wire_reset(){
    
-     DDRD|=0x10;         //PD4 output
-    PORTD&= (uint8_t) ~(1U<<4);   //PD4=0
+    DDRD =  1<<4;         //PD4 output
+    PORTD =  ~(1<<4);   //PD4=0
     
     _delay_ms(0.48);   //480usec
 
-    DDRD&=(uint8_t) ~(1U<<4);       //PD4 input
-    PORTD&= (uint8_t) ~(1U<<4);   //disable pull-up
+    DDRD =  ~(1<<4);        //PD4 input
+    PORTD =  ~(1<<4);   //disable pull-up
     
     _delay_ms(0.1);  //wait 100 usec for connected devices to transmit the presence pulse
             
@@ -24,8 +25,7 @@ uint8_t one_wire_reset(){
     
     if((output & (1<<PD4))== 0x00 )  return 1; //PD4 =0 connected device is detected
     
-    else
-    {return 0;  } //connected device isn't detected
+    return 0;   //connected device isn't detected
        
     
   }
@@ -33,13 +33,13 @@ uint8_t one_wire_reset(){
 
 uint8_t one_wire_receive_bit(){
 
-    DDRD|=0x10;   //PD4 as output
-    PORTD&= (uint8_t) ~(1U<<4);
+    DDRD=0x10;   //PD4 as output
+    PORTD=~(1<<PD4);
     _delay_ms(0.002);
     
     
-    DDRD&= (uint8_t) ~(1U<<4);     //PD4 input
-    PORTD&= (uint8_t) ~(1U<<4);   //disable pull-up
+    DDRD=0x00;       //PD4 input
+    PORTD=~(1<<PD4);   //disable pull-up
     
     _delay_ms(0.01);
     
@@ -49,22 +49,30 @@ uint8_t one_wire_receive_bit(){
     
     _delay_ms(0.049);
     return 0;  //0 is read
-    }}
+    }
+
+    else{
+    _delay_ms(0.049);
+    return 1; //1 is read
+    }    
+
+}
 
 void one_wire_transmit_bit(uint8_t bit){
- DDRD|=0x10;   //PD4 output
+
+    DDRD=0x10;   //PD4 output
     
-    PORTD=&= (uint8_t) ~(1U<<4);
+    PORTD=~(1<<PD4);
     
     _delay_ms(0.002);
     
-    if(bit == 0x00 ) PORTD&= (uint8_t) ~(1U<<4);
-    else if(bit == 0x01) PORTD|=(1<<PD4);
+    if(bit == 0x00 ) PORTD=~(1<<PD4);
+    else if(bit == 0x01) PORTD=(1<<PD4);
     
     _delay_ms(0.058);  //wait 58 usec for connected device to sample line 
    
     DDRD=0x00;   //PD4 input
-    PORTD&= (uint8_t) ~(1U<<4); //PD4 off to disable pull-up
+    PORTD=~(1<<PD4); //PD4 off to disable pull-up
     
     _delay_ms(0.001);  //recovery time
     
@@ -72,7 +80,7 @@ void one_wire_transmit_bit(uint8_t bit){
 
 uint8_t one_wire_receive_byte(){
 
- uint8_t received_data=0x00;
+    uint8_t received_data=0x00;
     
     uint8_t aux;
     
@@ -80,10 +88,14 @@ uint8_t one_wire_receive_byte(){
     for(int i=0; i<8; i++){
     
        aux=one_wire_receive_bit();
-       aux=aux<<i;
+       aux = aux<<i;
        received_data|=aux;
+      
+     }
+
+     return received_data;
 }
-}
+
 void one_wire_transmit_byte(uint8_t data){
 
     uint8_t aux;
@@ -96,3 +108,4 @@ void one_wire_transmit_byte(uint8_t data){
          }
 
 }
+
